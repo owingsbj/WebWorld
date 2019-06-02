504728cfea2601dbb9ad25978d247d8ecc3e7981
owingsbj <owingsbj@yahoo.com>
2019-06-02 12:36:49 -0700

    translucency mask property

--- src/com/gallantrealm/webworld/model/Translucency.java
+++ src/com/gallantrealm/webworld/model/Translucency.java
@@ -4,5 +4,14 @@ import com.gallantrealm.myworld.model.WWTranslucency;
 
 public class Translucency extends WWTranslucency {
 	private static final long serialVersionUID = 1L;
-
+	
+	public long getTranslucencyMask() {
+		return super.getInsideColor();
+	}
+	
+	public void setTranslucencyMask(long insideColor) {
+		super.setInsideColor((int)(insideColor & 0xFFFFFF));
+		super.setInsideTransparency((insideColor >> 24) & 0xFF);
+	}
+	
 }
