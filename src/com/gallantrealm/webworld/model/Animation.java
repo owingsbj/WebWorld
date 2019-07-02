fdc90ad1f424e29d0a5e8a1ca980d5645c59a0c9
owingsbj <owingsbj@yahoo.com>
2019-07-02 12:39:29 -0700

    animation frameworks

--- src/com/gallantrealm/webworld/model/Animation.java
+++ src/com/gallantrealm/webworld/model/Animation.java
@@ -1,22 +1,79 @@
 package com.gallantrealm.webworld.model;
 
-import com.gallantrealm.myworld.model.WWAnimation;
 import com.gallantrealm.myworld.model.WWObject;
 import com.gallantrealm.myworld.model.WWVector;
 
-public class Animation extends WWAnimation {
+public class Animation {
 	private static final long serialVersionUID = 1L;
 
-	@Override
-	public void getAnimatedPosition(WWObject object, WWVector position, long time) {
-		// TODO Auto-generated method stub
+	private String type;
+	private float speed;
+	private float range;
+	private transient boolean started;
+	private transient long startTime;
 
+	public Animation(String type) {
+		this.type = type;
+		this.speed = 1.0f;
+		this.range = 1.0f;
 	}
 
-	@Override
-	public void getAnimatedRotation(WWObject object, WWVector rotation, long time) {
-		// TODO Auto-generated method stub
+	public Animation(String type, float speed, float range) {
+		this.type = type;
+		this.speed = speed;
+		this.range = range;
+	}
 
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public float getSpeed() {
+		return speed;
+	}
+	public void setSpeed(float speed) {
+		this.speed = speed;
+	}
+
+	public float getRange() {
+		return range;
+	}
+	public void setRange(float range) {
+		this.range = range;
+	}
+
+	public void start() {
+		started = true;
+		startTime = System.currentTimeMillis();
+	}
+
+	public void stop() {
+		started = false;
+	}
+
+	public void animatePosition(WWObject object, WWVector position, long time) {
+		if (started) {
+			float animationTime = (int)(time - startTime) * speed;
+			if (type.equals("walk")) {
+
+			} else {
+
+			}
+		}
+	}
+
+	public void animateRotation(WWObject object, WWVector rotation, long time) {
+		if (started) {
+			float animationTime = (int)(time - startTime) * speed;
+			if (type.equals("walk")) {
+
+			} else {
+
+			}
+		}
 	}
 
 }
--- src/com/gallantrealm/webworld/model/EventBehavior.java
+++ src/com/gallantrealm/webworld/model/EventBehavior.java
@@ -1,12 +1,12 @@
 package com.gallantrealm.webworld.model;
 
 import org.mozilla.javascript.Function;
-import com.gallantrealm.myworld.model.WWBehavior;
+import com.gallantrealm.myworld.model.WWAnimation;
 import com.gallantrealm.myworld.model.WWEntity;
 import com.gallantrealm.myworld.model.WWObject;
 import com.gallantrealm.myworld.model.WWVector;
 
-public class EventBehavior extends WWBehavior {
+public class EventBehavior extends WWAnimation {
 	private static final long serialVersionUID = 1L;
 
 	public Function onTouch;
@@ -17,6 +17,7 @@ public class EventBehavior extends WWBehavior {
 	public Function onSlide;
 	public Function onStopSlide;
 	public Function onTimer;
+	public Animation[] animations;
 	
 	@Override
 	public boolean touchEvent(WWObject object, WWEntity toucher, int side, float x, float y) {
@@ -96,6 +97,24 @@ public class EventBehavior extends WWBehavior {
 			return true;
 		}
 		return false;
+	}
+
+	@Override
+	public void getAnimatedPosition(WWObject object, WWVector position, long time) {
+		if (animations != null) {
+			for (int i = 0; i < animations.length; i++) {
+				animations[i].animatePosition(object, position, time);
+			}
+		}
+	}
+
+	@Override
+	public void getAnimatedRotation(WWObject object, WWVector rotation, long time) {
+		if (animations != null) {
+			for (int i = 0; i < animations.length; i++) {
+				animations[i].animateRotation(object, rotation, time);
+			}
+		}
 	}
 
 }
