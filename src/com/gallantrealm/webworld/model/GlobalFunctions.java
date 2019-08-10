88649e87b330d9a7199961cc969595da9f216863
owingsbj <owingsbj@yahoo.com>
2019-08-10 13:44:20 -0700

    remove 6 item limitation from select function

--- src/com/gallantrealm/webworld/model/GlobalFunctions.java
+++ src/com/gallantrealm/webworld/model/GlobalFunctions.java
@@ -47,96 +47,36 @@ public class GlobalFunctions implements Serializable {
 		return returnedValue;
 	}
 	
-	static String[] selections;
-	static String[] imageFileNames;
+	static Selection[] selections;
 	
-	static class Selection1 {
-		public static String getStaticName() {
-			return selections[0];
+	static class Selection {
+		String name;
+		String imageFileName;
+		public Selection(String name, String imageFileName) {
+			this.name = name;
+			this.imageFileName = imageFileName;
 		}
-		public static String getStaticImageFileName() {
-			return imageFileNames[0];
+		public String getName() {
+			return name;
 		}
-	}
-	
-	static class Selection2 {
-		public static String getStaticName() {
-			return selections[1];
-		}
-		public static String getStaticImageFileName() {
-			return imageFileNames[1];
-		}
-	}
-	
-	static class Selection3 {
-		public static String getStaticName() {
-			return selections[2];
-		}
-		public static String getStaticImageFileName() {
-			return imageFileNames[2];
-		}
-	}
-	
-	static class Selection4 {
-		public static String getStaticName() {
-			return selections[3];
-		}
-		public static String getStaticImageFileName() {
-			return imageFileNames[3];
-		}
-	}
-	
-	static class Selection5 {
-		public static String getStaticName() {
-			return selections[4];
-		}
-		public static String getStaticImageFileName() {
-			return imageFileNames[4];
-		}
-	}
-	
-	static class Selection6 {
-		public static String getStaticName() {
-			return selections[5];
-		}
-		public static String getStaticImageFileName() {
-			return imageFileNames[5];
+		public String getImageFileName() {
+			return imageFileName;
 		}
 	}
 	
 	public static String select(String message, NativeArray jsSelections, NativeArray jsImageFileNames) {
 		AndroidClientModel clientModel = AndroidClientModel.getClientModel();
-		selections = new String[jsSelections.size()];
+		selections = new Selection[jsSelections.size()];
 		for (int i = 0; i < selections.length; i++) {
-			selections[i] = String.valueOf(jsSelections.get(i));
-		}
-		imageFileNames = new String[jsImageFileNames.size()];
-		for (int i = 0; i < imageFileNames.length; i++) {
-			imageFileNames[i] = Texture.worldPrefixUrl(String.valueOf(jsImageFileNames.get(i)));
-		}
-		Class[] availableItems = new Class[selections.length];
-		for (int i = 0; i < selections.length; i++) {
-			if (i == 0) {
-				availableItems[i] = Selection1.class;
-			} else if (i == 1) {
-				availableItems[i] = Selection2.class;
-			} else if (i == 2) {
-				availableItems[i] = Selection3.class;
-			} else if (i == 3) {
-				availableItems[i] = Selection4.class;
-			} else if (i == 4) {
-				availableItems[i] = Selection5.class;
-			} else if (i == 5) {
-				availableItems[i] = Selection6.class;
-			}
+			selections[i] = new Selection(String.valueOf(jsSelections.get(i)), Texture.worldPrefixUrl(String.valueOf(jsImageFileNames.get(i))));
 		}
 		returnedValue = null;
 		final Thread thisThread = Thread.currentThread();
-		clientModel.selectAlert(message, availableItems, new String[] { "OK", "Cancel" }, new SelectResponseHandler() {
-			public void handleSelect(Class selectedItem, int option) {
+		clientModel.selectAlert(message, selections, null, new SelectResponseHandler() {
+			public void handleSelect(Object selectedItem, int option) {
 				if (option == 0) {
 					try {
-						returnedValue = (String)selectedItem.getMethod("getStaticName").invoke(null);
+						returnedValue = ((Selection)selectedItem).getName();
 					} catch (Exception e) {
 						returnedValue = null;;
 					}
