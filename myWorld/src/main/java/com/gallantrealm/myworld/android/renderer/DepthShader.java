790286d11965f7784b396d86507356eb598669be
BJ Owings <bj@gallantrealm.com>
2023-01-09 21:49:46 -0800

    match depth shader precision with other shaders

--- myWorld/src/main/java/com/gallantrealm/myworld/android/renderer/DepthShader.java
+++ myWorld/src/main/java/com/gallantrealm/myworld/android/renderer/DepthShader.java
@@ -1,9 +1,13 @@
 package com.gallantrealm.myworld.android.renderer;
 
+/**
+ * This shader is used to fill the depth dimension so that the texture shaders will run more optimally, avoiding invoking
+ * the fragement shading for pixels that will not be visible (because the pixel will be redrawn
+ */
 public class DepthShader extends Shader {
 	
 	public static final String vs = "\n" +
-			"precision mediump float; \n" +
+			"precision highp float; \n" +
 			"\n" +
 			"attribute vec4 aPosition; \n" +
 			"uniform mat4 mvMatrix; \n" +
@@ -13,7 +17,7 @@ public class DepthShader extends Shader {
 			"}";
 	
 	public static final String fs = "\n" +
-			"precision mediump float; \n" +
+			"precision highp float; \n" +
 			"\n" +
 			"void main() { \n" +
 			"	gl_FragColor = vec4(1.0); // needed or linker errors sometimes \n" +
