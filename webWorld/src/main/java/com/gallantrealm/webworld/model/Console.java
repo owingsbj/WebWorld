21303d3b973aa0d000fdccf9874adc027b5ad187
BJ Owings <bj@gallantrealm.com>
2023-01-25 23:49:41 -0800

    changing Console methods to statics

--- webWorld/src/main/java/com/gallantrealm/webworld/model/Console.java
+++ webWorld/src/main/java/com/gallantrealm/webworld/model/Console.java
@@ -6,29 +6,29 @@ import com.gallantrealm.myworld.android.AndroidClientModel;
 public class Console implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
-	public void log(String message) {
+	public static void log(String message) {
 		AndroidClientModel.getClientModel().log(message);
 	}
 	
-	public void debug(String message) {
+	public static void debug(String message) {
 		if (AndroidClientModel.getClientModel().isShowDebugLogging()) {
 			log("DEBUG: "+message);
 		}
 	}
 	
-	public void info(String message) {
+	public static void info(String message) {
 		log("INFO: "+message);
 	}
 	
-	public void warn(String message) {
+	public static void warn(String message) {
 		log("WARNING: "+message);
 	}
 	
-	public void error(String message) {
+	public static void error(String message) {
 		log("ERROR: "+message);
 	}
 	
-	public void assertt(boolean condition, String message) {
+	public static void assertt(boolean condition, String message) {
 		if (condition) {
 			if (AndroidClientModel.getClientModel().isShowDebugLogging()) {
 				log("ASSERT: "+message);
