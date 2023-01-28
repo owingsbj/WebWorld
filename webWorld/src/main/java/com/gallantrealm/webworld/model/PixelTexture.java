807b440d73666f2da7ca270b2530d843b08dacec
BJ Owings <bj@gallantrealm.com>
2023-01-28 11:02:13 -0800

    Adding PixelTexture helper

--- webWorld/src/main/java/com/gallantrealm/webworld/model/PixelTexture.java
+++ webWorld/src/main/java/com/gallantrealm/webworld/model/PixelTexture.java
@@ -3,6 +3,6 @@ package com.gallantrealm.webworld.model;
 public class PixelTexture extends Texture {
 
 	public PixelTexture(String name, int totalWidth, int totalHeight, int width, int height, float rotation,  int offsetX, int offsetY) {
-		super(name, totalWidth / (float)width, totalHeight / (float)height, rotation, width / (float)offsetX, height / (float)offsetY, true);
+		super(name, totalWidth / (float)width, totalHeight / (float)height, rotation, offsetX / (float)width, offsetY / (float)height, true);
 	}
 }
